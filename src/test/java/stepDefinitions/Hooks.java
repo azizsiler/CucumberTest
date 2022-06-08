6f912e03e6306924ee910d5b60334f517bba979e
azizsiler <azizsiler@gmail.com>
2022-06-08 22:34:11 +0300

    Initial commit

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -1,6 +1,43 @@
 package stepDefinitions;
 
+<<<<<<< HEAD
 public class Hooks {
 
     // stepdefinitions package icerisinde @Bifore ve@After metho
 }
+=======
+import io.cucumber.java.Scenario;
+import org.junit.After;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import utilities.Driver;
+
+public class Hooks {
+
+    @After
+    public void tearDown(Scenario scenario){
+        final byte[] screenshot=((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+        if (scenario.isFailed()) {
+            scenario.attach(screenshot, "image/png","screenshots");
+        }
+        Driver.closeDriver();
+    }
+}
+        /*
+        Cucumber'da step definitions package'i içerisinde
+@before @after gibi bir notasyon varsa
+extends testBase dememize gerek kalmadan
+her scenario'dan önce ve/veya sonra bu methodlar çalışacaktır
+
+Bu da bizim isteyeceğimiz bir durum değildir
+
+Cucumber'da @before @after kullanma ihtiyacımız olursa
+bunu stepDefinitions package'i altında oluşturacağımız
+Hooks class'ına koyarız
+
+Biz her scenario'dan sonra test sonucunu kontrol edip failed olan
+scenario'lar için screenshoot olmasi amaciyla
+@After methodu kullanacağız
+
+ */
+>>>>>>> fdaff37 (Initial commit)
